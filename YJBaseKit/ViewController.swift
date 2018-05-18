a4a594294990140a5c1eb7ab1c60671da9cefa4b
朱亚静 <zhuyajing@zhuyajingdeimac.local>
2018-05-18 08:57:15 +0800

    添加工程

---
+++ YJBaseKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YJBaseKit
+//
+//  Created by 朱亚静 on 2018/5/18.
+//  Copyright © 2018年 朱亚静. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
